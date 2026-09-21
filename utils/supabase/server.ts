a6de5bf2ee3d98f8dd44f30cf13f77927deef0cf
ronmardi <4ng9s4vtmv@privaterelay.appleid.com>
2026-09-21 17:15:31 -0300

    server 2

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,8 +1,10 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
-  const cookieStore = cookies()
+// 1. Agregamos "async" a la función
+export async function createClient() {
+  // 2. Agregamos "await" antes de llamar a cookies()
+  const cookieStore = await cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
